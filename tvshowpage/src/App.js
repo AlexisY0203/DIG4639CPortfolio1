5bc613a74f2e8bda04753991b27eb10de4651887
Alexis Yarbrough <alexisy0203@knights.ucf.edu>
2022-03-16 17:24:35 -0400

    App.js changes

--- tvshowpage/src/App.js
+++ tvshowpage/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import ShortBio from './components/ShortBio';
+import SomePosts from './components/SomePosts';
+import React, {useState} from 'react'
+
+const showterms = {
+  "show":"New Girl",
+  "mainCharacter": "Jessica Day",
+  "year": 2011,
+  "seasons": "seven"
+}
 
 function App() {
+  let [displayComponents, setDisplayComponents] = useState(-1)
+  let components = [<ShortBio data={showterms}></ShortBio>,<SomePosts></SomePosts>]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    (displayComponent !== -1 ?
+      <div className="App">
+      {displayComponent !== -1 ? components[displayComponent] : undefined}
+      <button onClick={() => setDisplayComponent(-1)}>Back</button>
+      </div>
+    :
+      <div className="App">
+      <h1>What is my favorite show?</h1>
+      <p>Choose a Topic:</p>
+      <p>
+        <button onClick={() => setDisplayComponent(0)}>Short Bio</button>
+      </p>
+      <p>
+        <button onClick={() => setDisplayComponent(1)}>Some Posts</button>
+      </p>  
+      </div>
+    )
   );
 }
 
