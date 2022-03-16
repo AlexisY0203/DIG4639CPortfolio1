1f241d9a48bb9e6791f38798fb268e6786935cf6
Alexis Yarbrough <alexisy0203@knights.ucf.edu>
2022-03-16 17:21:27 -0400

    ShortBio changes

---
+++ tvshowpage/src/components/ShortBio/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class ShortBio extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>My favorite show is...</h1>
+                <h2>{this.props.data.show}</h2>
+                <p><img src="/img/newGirlPic.jpeg" alt="New Girl Cast" width="50%"></img></p>
+                <p>{this.props.data.show} is an American sitcom from {this.props.data.year} with {this.props.data.seasons} seasons.</p>
+                <p>Along with this knowledge, my favorite character from it is {this.props.data.mainCharacter}.</p>
+            </>
+        )
+    }
+}
