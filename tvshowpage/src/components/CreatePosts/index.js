ce8f9a0b2fdf4cd5cb2ad114c7107fe548d38f45
Alexis Yarbrough <alexisy0203@knights.ucf.edu>
2022-03-21 15:46:54 -0400

    img correction

--- tvshowpage/src/components/CreatePosts/index.js
+++ tvshowpage/src/components/CreatePosts/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Post from '../Post'
+import img1 from './img/sadjess.jpg'
 
 let contentArray = [
     {title:'Hard Times', content:"The economy stinks, bees are dying, and movies are pretty much all sequels now. -Schmidt", index:1},
     {title:"Self Care", content:"I am not a successful adult. I don't eat vegetables and/or take care of myself. -Nick Miller", index:2},
     {title:"Dance Party", content:<img src='/img/dancing.gif' alt='Cast of New Girl Dancing' height='300px' />, index:3},
-    {title:"Sad Times", content:<img src='/img/sadjess.jpg' alt='Jessica Day Crying' height='300px' />, index:4}
+    {title:"Sad Times", content:<img src={img1} alt='Jessica Day Crying' height='300px' />, index:4}
 ]
 
 export default class CreatePosts extends React.Component {
