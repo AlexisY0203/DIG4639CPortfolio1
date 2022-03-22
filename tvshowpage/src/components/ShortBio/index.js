ac765984a5e2d97244a3697f7b528bbb4fde77fd
Alexis Yarbrough <alexisy0203@knights.ucf.edu>
2022-03-21 20:23:28 -0400

    img importing

--- tvshowpage/src/components/ShortBio/index.js
+++ tvshowpage/src/components/ShortBio/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import cast from '../img/newGirlPic.jpeg';
 
 export default class ShortBio extends React.Component {
     render() {
@@ -6,7 +7,7 @@ export default class ShortBio extends React.Component {
         <>
             <h1>My favorite show is.....</h1>
             <h2>{this.props.data.show}</h2>
-            <img src="/img/newGirlPic.jpeg" alt="New Girl Cast" width="50%"></img>
+            <img src={cast} alt="New Girl Cast" width="50%"></img>
             <p>{this.props.data.show} is an American sitcom from {this.props.data.year} with {this.props.data.seasons} seasons.</p>
             <p>My favorite character from it is {this.props.data.mainCharacter}.</p>
         </>
